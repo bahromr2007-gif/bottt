25b039413a0b7bc88b317d4b26e9363f3362aee1
bahromr2007-gif <bahromr2007@gmail.com>
2026-04-06 00:57:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1498,4 +1498,4 @@ ensure_demo_products()
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
