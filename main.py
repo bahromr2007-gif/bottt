3a03ac85937e19cf58f3ded50e7f205b3e374c3e
bahromr2007-gif <bahromr2007@gmail.com>
2026-04-06 01:20:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from typing import Dict, List, Optional, Any
 # Eslatma:
 # Pastdagi qiymatlarni o'zingiznikiga almashtiring.
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8778358404:AAHmM4e2OnROyXLCsGXERCbXd3arzl7kPS0")
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://your-render-app.onrender.com")
+RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL", "https://bottt-02j7.onrender.com")
 WEB_APP_URL = f"{RENDER_EXTERNAL_URL}/shop"
 ADMIN_IDS = [8735360012]
 
